e3a83335d16232b8489b0c54cafd02c0297fc31e
Luis Eduardo de Oliveira Floriano <eduardofloriano@outlook.com>
2024-09-28 13:48:47 +0000

    Trigger 2

--- infra/modules/instancia-ec2/outputs.tf
+++ infra/modules/instancia-ec2/outputs.tf
@@ -1,4 +1,4 @@
 output "instancia-ec2-id" {
-  description = "O ID da Instancia ec2 no console"
+  description = "O ID da Instancia ec2 no console 2"
   value = aws_instance.instancia-ec2-mack.id 
 }
